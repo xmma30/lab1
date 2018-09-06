2c74a6fd5d4430671b28eddafcb79cadfe290930
xmma <xmma@uvm.edu>
2018-09-06 11:21:00 -0400

    first commit

---
+++ app/src/main/java/com/example/xmma/simplest/SimplestActivity.java
@@ -0,0 +1,13 @@
+package com.example.xmma.simplest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SimplestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_simplest);
+    }
+}
